a2e682cfa1f3e03cef0d50cc3ffd25eb6f12d697
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-04-28 12:38:02 +0800

    Revert "Loads all languages associated to a project"
    This reverts commit c3a43af95ff0801508493b333fed2814f9e0730a.

--- src/Content/Projects.purs
+++ src/Content/Projects.purs
@@ -4,9 +4,8 @@ import Data.Map
 
 import Assets as A
 import Category (categoryHidden)
-import Data.Argonaut (Json)
+import Data.Argonaut (Json, jsonParser)
 import Data.Argonaut.Decode (decodeJson)
-import Data.Array (catMaybes)
 import Data.Either (Either, either)
 import Data.Maybe (fromMaybe, maybe, Maybe(..))
 import Data.Tuple.Nested ((/\))
@@ -16,20 +15,14 @@ import Halogen.HTML.Properties as HP
 import Halogen.HTML.Properties.ARIA as ARIA
 import Halogen.Themes.Bootstrap4 as BS
 import Lists (ListItem, listGroupC, listItem_)
-import Prelude (map, (>>>), (>>=), flip, (#), ($), (<#>), (<$>))
+import Prelude (map, (>>>), (>>=), flip, (#), ($))
 
-type ProjectJSON
+type Project
   = { html_url :: String
     , name :: String
     , description :: Maybe String
     , language :: Maybe String
-    , languages_url :: Maybe String
     , homepage :: Maybe String
-    }
-
-type Project 
-  = { project :: ProjectJSON
-    , languages :: Array String
     }
 
 icons :: Map String String
@@ -46,45 +39,48 @@ icons =
     , "Elm" /\ A.elmIcon
     , "HTML" /\ A.htmlIcon
     , "CSS" /\ A.cssIcon
-    , "Python" /\ A.pythonIcon
-    , "C#" /\ A.csharpIcon
-    , "Lua" /\ A.luaIcon
     ]
 
-jsonToProject :: Json -> Either String (Array ProjectJSON)
+jsonToProject :: Json -> Either String (Array Project)
 jsonToProject = decodeJson
 
 mkProject :: forall w i. Project -> ListItem w i
 mkProject pro =
   let
-    iconDivs :: Array String -> Array (HH.HTML w i)
-    iconDivs l =
-      (l <#> flip lookup icons)
-        <#> (\language -> language >>= (\l' -> Just $ A.iconS l' [ HP.class_ (HH.ClassName "projectIcon") ]))
-        # catMaybes
+    iconDiv =
+      pro.language
+        >>= flip lookup icons
+        # maybe (HH.text "") (\language -> A.iconS language [ HP.class_ (HH.ClassName "projectIcon") ])
   in
     listItem_
       [ HH.div
           [ HP.class_ BS.row ]
-          [ HH.div [ HP.classes [ BS.col ] ]
-              (iconDivs pro.languages)
-          , HH.div [ HP.classes [ BS.col ] ]
-              [ HH.a [ HP.href pro.project.html_url ] [ HH.text pro.project.name ] ]
-          , HH.div [ HP.classes [ BS.col ] ]
-            [ F.para $ fromMaybe "" pro.project.description ]
+          [ HH.div [ HP.class_ BS.col1 ]
+              [ iconDiv ]
+          , HH.div [ HP.class_ BS.col ]
+              [ HH.a [ HP.href pro.html_url ] [ HH.text pro.name ] ]
+          , HH.div [ HP.class_ BS.col ]
+            [ F.para $ fromMaybe "" pro.description ]
           ]
       ]
 
-projects :: forall w i. Maybe (Array Project) -> HH.HTML w i
+projects :: forall w i. Maybe String -> HH.HTML w i
 projects Nothing =
   categoryHidden "projects" "Projects"
     [ HH.div [ HP.class_ BS.spinnerBorder, ARIA.role "status" ] [ HH.span [ HP.class_ BS.srOnly ] [] ] ]
 
 projects (Just s) =
   let
+    parsed = jsonParser s
 
-    prjkts = map mkProject s
+    prjkts = either mkErrorMsg decodeThenMk parsed
   in
     categoryHidden "projects" "Projects"
       [ listGroupC [ BS.textLeft ] prjkts
       ]
+  where
+  decodeThenMk :: forall x j. Json -> Array (ListItem x j)
+  decodeThenMk = jsonToProject >>> either mkErrorMsg (map mkProject)
+
+  mkErrorMsg :: forall x j. String -> Array (ListItem x j)
+  mkErrorMsg err = [ listItem_ [ HH.text err ] ]
--- src/Resume.purs
+++ src/Resume.purs
@@ -3,25 +3,21 @@ module Resume (component) where
 import Affjax as AX
 import Affjax.ResponseFormat as AXRF
 import Content.Skills as S
-import Data.Argonaut (Json, jsonParser, toObject)
-import Data.Either (either, hush)
-import Data.Maybe (Maybe(..), maybe, fromMaybe)
-import Data.Traversable (sequence, traverse)
+import Data.Either (either)
+import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff)
-import Foreign.Object as FO
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import Halogen.Themes.Bootstrap4 as BS
 import Languages as L
 import Personal (personalInformation)
-import Prelude (Unit, const, ($), (>>>), bind, pure, map, (>>=), (#), (<$>), (<#>))
-import Projects (ProjectJSON, jsonToProject)
-import Projects as P
+import Prelude (Unit, const, ($), (>>>), bind)
+import Projects (projects)
 import Work as W
 
 type State
-  = Maybe (Array P.Project)
+  = Maybe String
 
 data Action
   = Init
@@ -44,7 +40,7 @@ render state =
 
     skill =
       HH.div [ HP.classes [ BS.colMd8, BS.colSm7, BS.col12 ] ]
-        [ W.workExperience, S.technicalSkills, P.projects state ]
+        [ W.workExperience, S.technicalSkills, projects state ]
   in
     HH.div [ HP.class_ BS.containerFluid ]
       [ HH.div [ HP.class_ BS.row ]
@@ -54,36 +50,6 @@ render state =
       ]
 
 handleAction :: forall o m. MonadAff m => Action -> H.HalogenM State Action ChildSlots o m Unit
-handleAction Init = do
+handleAction Init = do 
   response <- H.liftAff $ AX.get AXRF.string "https://api.github.com/users/de-passage/repos?sort=updated"
-  let
-    marr = do
-      r <- hush response
-      values <- hush $ jsonParser r.body
-      hush $ jsonToProject values
-
-  projects <- getProjects marr
-  H.put projects
-  where
-  getProjects :: Maybe (Array P.ProjectJSON) -> H.HalogenM State Action ChildSlots o m (Maybe (Array P.Project))
-  getProjects ma = case ma of
-                    Nothing -> pure Nothing 
-                    Just a -> buildProjects a <#> Just
-
-  buildProjects :: Array P.ProjectJSON -> H.HalogenM State Action ChildSlots o m (Array P.Project)
-  buildProjects = traverse $ \proj ->
-      case proj.languages_url of
-        Nothing -> pure $ { project: proj, languages: [] }
-        Just url ->  getLangs url <#> (\a -> { project: proj, languages: a })
-
-  getLangs :: String -> H.HalogenM State Action ChildSlots o m (Array String)
-  getLangs url = fetchLangs url <#> parseLangs
-
-  fetchLangs :: String -> H.HalogenM State Action ChildSlots o m (Maybe (AX.Response String))
-  fetchLangs url = H.liftAff (AX.get AXRF.string url <#> hush)
-
-  parseLangs :: forall r. Maybe { body :: String | r } -> Array String
-  parseLangs j = j >>= _.body >>> jsonParser >>> hush <#> jsonToLangs # fromMaybe []
-
-  jsonToLangs :: Json -> Array String
-  jsonToLangs json = toObject json <#> FO.keys # fromMaybe []
+  H.put (either (const Nothing) (_.body >>> Just) response)
